9944ad90eb0881761633a7ad181e4168616f0009
John Shier <79332140+Array0x@users.noreply.github.com>
2022-05-12 12:04:49 -0400

    new EXCITING handler

--- commands/item.js
+++ commands/item.js
@@ -1,11 +1,11 @@
 // Please use the GiveResults() Function. DO NOT USE OUR OLD METHOD:
 module.exports = {
-    name: 'item',
-    decription: "Gives path to items",
+    name: 'object',
+    decription: "Gives path to objects",
     execute(message, args, Discord) {
         const { MessageEmbed, MessageAttachment } = require('discord.js');
         const blueprints = require('../jsons/bp.json');
-        const SayMessage = message.content.slice(5).trim();
+        const SayMessage = message.content.slice(7).trim();
 
         // Epic Function
         function GiveResults(Name, Path, Despawns, Command, Image)
@@ -23,17 +23,21 @@ module.exports = {
             message.channel.send(ItemEmbed);
         }
 
-        if(SayMessage == "lars van")
-        {
-            // NEW METHOD:
+        // New exciting handler
+        if(SayMessage == "lars van") {
             GiveResults(blueprints.lars_name, blueprints.lars_path, blueprints.lars_despawns, blueprints.lars_path, 'https://media.discordapp.net/attachments/973912253554360390/973912323213365348/larsvan.jpg?width=960&height=540');
         } else if(SayMessage == "storm shield") {
-            // NEW METHOD:
             GiveResults(blueprints.stormshield_name, blueprints.stormchest_path, blueprints.stormshield_despawns, blueprints.stormshield_path, 'https://d1fs8ljxwyzba6.cloudfront.net/assets/article/2017/07/27/Fortnite-how-to-increase-homebase-power-level_feature.jpg');
         } else if(SayMessage == "storm chest") {
             GiveResults(blueprints.stormchest_name, blueprints.stormchest_path, blueprints.stormchest_despawns, blueprints.stormchest_path, 'https://static.wikia.nocookie.net/fortnite/images/3/3d/Storm_Chest_-_Locked_-_Fortnite.jpg/revision/latest?cb=20211201072445');
+        } else if(SayMessage == "gliding") {
+            GiveResults(blueprints.gliding_name, blueprints.gliding_path, blueprints.gliding_despawns, blueprints.gliding_path, 'https://progameguides.com/wp-content/uploads/2020/02/fortnite-outfit-agent-peely-ghost.jpg');
+        } else if(SayMessage == "radar tower hologram") {
+            GiveResults(blueprints.radar_name, blueprints.radar_path, blueprints.radar_despawns, blueprints.radar_path, 'https://static.wikia.nocookie.net/fortnite_gamepedia/images/5/5a/Radar_tower_blueprint_1.png/revision/latest/scale-to-width-down/1200?cb=20180314091536');
+        } else if(SayMessage == "bluglo") {
+            GiveResults(blueprints.bluglo_name, blueprints.bluglo_path, blueprints.bluglo_despawns, blueprints.bluglo_path, 'https://static.wikia.nocookie.net/fortnite_gamepedia/images/b/bd/Bluglo_icon.png/revision/latest/scale-to-width-down/1200?cb=20190624155404');
         } else {
-            message.channel.send("At this time, you can only get lars van. We are adding everything tonight");
+            message.channel.send("WHOOPS! This item is not available in our database yet. If you have the item path please send it to array0x#0001 to be added to the database");
         }
     }
 }
