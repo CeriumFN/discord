50dbfa42053d65dfc32edce528c8cfa2bde1932b
John Shier <79332140+Array0x@users.noreply.github.com>
2022-05-12 11:53:44 -0400

    made it a bit easier to read

--- commands/prop.js
+++ commands/prop.js
@@ -1,44 +1,37 @@
+// Please use the GiveResults() Function. DO NOT USE OUR OLD METHOD:
 module.exports = {
-    name: 'prop',
-    decription: "Gives path to props",
+    name: 'item',
+    decription: "Gives path to items",
     execute(message, args, Discord) {
         const { MessageEmbed, MessageAttachment } = require('discord.js');
-        // Get all objects locally
         const blueprints = require('../jsons/bp.json');
-        // trims message to make handler
         const SayMessage = message.content.slice(5).trim();
 
-        // Prop handler
+        // Epic Function
+        function GiveResults(Name, Path, Despawns, Command, Image)
+        {
+            const ItemEmbed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(Name)
+            .addFields(
+                { name: 'Name', value: Name },
+                { name: 'Path', value: "`" + Path + "`"},
+                { name: 'Despawns', value: Despawns},
+                { name: 'Command', value: '`cheatscript loadobject ' + Command + '`'},
+            )
+            .setImage(Image);
+            message.channel.send(ItemEmbed);
+        }
+
         if(SayMessage == "lars van")
         {
-            const file = new MessageAttachment("./images/items/larsvan.jpg");
-            const ItemEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle(blueprints.lars_name)
-            .addFields(
-                { name: 'Name', value: blueprints.lars_name },
-                { name: 'Path', value: "`" + blueprints.lars_path + "`"},
-                { name: 'Despawns', value: blueprints.lars_despawns},
-                { name: 'Command', value: '`cheatscript loadobject /Game/Missions/Primary/LaunchTheBalloon/Props/BP_RtL_Main.BP_RtL_Main_C`'},
-            )
-            .setImage('https://media.discordapp.net/attachments/973912253554360390/973912323213365348/larsvan.jpg?width=960&height=540');
-            message.channel.send(ItemEmbed);
-            //message.channel.send("Name: " + blueprints.lars_name);
-            //message.channel.send("Path: " + blueprints.lars_path);
-            //message.channel.send("Despawns: " + blueprints.lars_despawns);
+            // NEW METHOD:
+            GiveResults(blueprints.lars_name, blueprints.lars_path, blueprints.lars_despawns, blueprints.lars_path, 'https://media.discordapp.net/attachments/973912253554360390/973912323213365348/larsvan.jpg?width=960&height=540');
         } else if(SayMessage == "storm shield") {
-            const file = new MessageAttachment("./images/items/larsvan.jpg");
-            const ItemEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle(blueprints.lars_name)
-            .addFields(
-                { name: 'Name', value: blueprints.stormshield_name },
-                { name: 'Path', value: "`" + blueprints.stormshield_path + "`"},
-                { name: 'Despawns', value: blueprints.stormshield_despawns},
-                { name: 'Command', value: '`cheatscript loadobject /Game/Missions/Primary/Outpost/Props/BP_StormShield_Core_New.BP_StormShield_Core_N`'},
-            )
-            .setImage('https://d1fs8ljxwyzba6.cloudfront.net/assets/article/2017/07/27/Fortnite-how-to-increase-homebase-power-level_feature.jpg');
-            message.channel.send(ItemEmbed);
+            // NEW METHOD:
+            GiveResults(blueprints.stormshield_name, blueprints.stormchest_path, blueprints.stormshield_despawns, blueprints.stormshield_path, 'https://d1fs8ljxwyzba6.cloudfront.net/assets/article/2017/07/27/Fortnite-how-to-increase-homebase-power-level_feature.jpg');
+        } else if(SayMessage == "storm chest") {
+            GiveResults(blueprints.stormchest_name, blueprints.stormchest_path, blueprints.stormchest_despawns, blueprints.stormchest_path, 'https://static.wikia.nocookie.net/fortnite/images/3/3d/Storm_Chest_-_Locked_-_Fortnite.jpg/revision/latest?cb=20211201072445');
         } else {
             message.channel.send("At this time, you can only get lars van. We are adding everything tonight");
         }
